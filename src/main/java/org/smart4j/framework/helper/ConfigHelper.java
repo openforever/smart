3f3a9a4c4a6feade05bfbb96ff3ef3ace414178b
openforever <youlovemezky@gmail.com>
2016-04-17 11:35:59 +0800

    配置文件助手类，静态方法获取smart.properties文件中的配置项

---
+++ src/main/java/org/smart4j/framework/helper/ConfigHelper.java
@@ -0,0 +1,59 @@
+package org.smart4j.framework.helper;
+
+import org.smart4j.framework.ConfigConstant;
+import org.smart4j.framework.utils.PropsUtil;
+
+import java.util.Properties;
+
+/**
+ * Created by snow on 2016/4/17.
+ * 属性文件助手类,提供静态方法，分别获取smart.properties配置文件中的配置项
+ * @author openforever
+ * @since 1.0.0
+ */
+public final class ConfigHelper {
+    private static final Properties CONFIG_PROPS = PropsUtil.loadProps(ConfigConstant.CONFIG_FILE);
+
+
+    public static String getJdbcDriver(){
+        return PropsUtil.getString(CONFIG_PROPS, ConfigConstant.JDBC_DRIVER);
+    }
+
+    public static String getJdbcUrl(){
+        return PropsUtil.getString(CONFIG_PROPS, ConfigConstant.JDBC_URL);
+    }
+
+    public static String getJdbcUsername(){
+        return PropsUtil.getString(CONFIG_PROPS, ConfigConstant.JDBC_USERNAME);
+    }
+
+    public static String getJdbcPassword(){
+        return PropsUtil.getString(CONFIG_PROPS, ConfigConstant.JDBC_PASSWORD);
+    }
+
+    /**
+     * 获取应用基础包名
+     * @return
+     */
+    public static String getAppBasePackage(){
+        return PropsUtil.getString(CONFIG_PROPS, ConfigConstant.APP_BASE_PACKAGE);
+    }
+
+    /**
+     * 获取应用JSP路径,提供默认路径/WEB-INF/view
+     * 以smart.properties配置文件中所配置的值为优先值
+     * @return
+     */
+    public static String getAppJspPath(){
+        return PropsUtil.getString(CONFIG_PROPS, ConfigConstant.APP_JSP_PATH, "/WEB-INF/view");
+    }
+
+    /**
+     * 获取应用静态资源路径,默认路径/asset/
+     * 以smart.properties配置文件中所配置的值为优先值
+     * @return
+     */
+    public static String getAppAssetPath(){
+        return PropsUtil.getString(CONFIG_PROPS, ConfigConstant.APP_ASSET_PATH, "/asset/");
+    }
+}
