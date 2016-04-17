1e9b48f5a049829a2c9e0efd67664a11d7c3f90a
openforever <youlovemezky@gmail.com>
2016-04-17 17:22:02 +0800

    实现Bean容器,BEAN_MAP中保存了beanClass与bean的映射

---
+++ src/main/java/org/smart4j/framework/helper/BeanHelper.java
@@ -0,0 +1,47 @@
+package org.smart4j.framework.helper;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by snow on 2016/4/17.
+ * 相当于Bean容器, 此时一个beanClass类只有一个bean实例
+ * ①：获取smart框架管理的所有的Bean类，ClassHelper.getBeanClassSet()
+ * ②：循环调用ReflectionUtil.newInstance(beanClass)，根据类来实例bean对象
+ * ③：将每次创建的bean存放到一个静态的Map<beanClass, bean>中
+ * @author openforever
+ * @since 1.0.0
+ */
+public class BeanHelper {
+    /**
+     * 定义Bean映射(用于存放Bean类与Bean实例的映射关系)
+     */
+    private static final Map<Class<?>, Object> BEAN_MAP = new HashMap<Class<?>, Object>();
+
+    static {
+        Set<Class<?>> beanClassSet = ClassHelper.getBeanClassSet();/*返回所有的Bean Class*/
+        for (Class<?> beanClass : beanClassSet){
+            Object obj = ReflectionUtil.newInstance(beanClass);
+            BEAN_MAP.put(beanClass, obj);
+        }
+    }
+
+    /**
+     * 获取bean映射
+     * @return BEAN_MAP
+     */
+    public static Map<Class<?>, Object> getBeanMap(){
+        return BEAN_MAP;
+    }
+
+    /**
+     * 根据指定的class对象获取bean实例
+     */
+    public static <T> T getBean(Class<T> clazz){
+        if (!BEAN_MAP.containsKey(clazz)){
+            throw new RuntimeException("can not get bean by class: " + clazz);
+        }
+        return (T) BEAN_MAP.get(clazz);
+    }
+}
