a2b5d527a37ff88753294f053c2eb0fb6f39b596
openforever <youlovemezky@gmail.com>
2016-04-18 11:09:35 +0800

    利用JDK实现动态代理，这里只能代理实现了接口的类

---
+++ src/main/java/org/smart4j/framework/proxy/DynamicProxy.java
@@ -0,0 +1,53 @@
+package org.smart4j.framework.proxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+/**
+ * Created by snow on 2016/4/18.
+ * 利用JDK实现动态代理，这里只能代理有接口的类，代理一个没有任何接口的类，可以使用CGlib类库
+ * @author openforever
+ * @since 1.0.0
+ */
+public class DynamicProxy implements InvocationHandler {
+
+    /*被代理的目标对象*/
+    private Object target;
+
+    public DynamicProxy(Object target) {
+        this.target = target;
+    }
+
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        before();
+        Object result = method.invoke(target, args);
+        after();
+        return result;
+    }
+
+
+    /**
+     * 封装了Proxy.newProxyInstance方法(无需传入参数)，使用泛型T，Object向下转型为T，所以IDE有警告
+     * @param <T>
+     * @return proxy
+     */
+    @SuppressWarnings("unchecked")
+    public <T> T getProxy(){
+        return (T) Proxy.newProxyInstance(
+                target.getClass().getClassLoader(), /*类加载器*/
+                target.getClass().getInterfaces(), /*该实现类的所有接口*/
+                this /*代理target对象的代理类*/
+                );
+    }
+
+    private void after() {
+
+    }
+
+    private void before() {
+
+    }
+
+
+}
