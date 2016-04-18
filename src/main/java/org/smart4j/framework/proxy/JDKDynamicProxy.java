b609e2a17cd3be46d501059b0dac61fa5a85b1ab
openforever <youlovemezky@gmail.com>
2016-04-18 16:26:07 +0800

    利用JDK自带的实现来实现动态代理 ，result为通过反射执行方法后的返回值

--- src/main/java/org/smart4j/framework/proxy/JDKDynamicProxy.java
+++ src/main/java/org/smart4j/framework/proxy/JDKDynamicProxy.java
@@ -6,23 +6,24 @@ import java.lang.reflect.Proxy;
 
 /**
  * Created by snow on 2016/4/18.
+ * 所有的代理类都合并到了JDKDynamicProxy动态代理类中
  * 利用JDK实现动态代理，这里只能代理有接口的类，代理一个没有任何接口的类，可以使用CGlib类库
  * @author openforever
  * @since 1.0.0
  */
-public class DynamicProxy implements InvocationHandler {
+public class JDKDynamicProxy implements InvocationHandler {
 
     /*被代理的目标对象*/
     private Object target;
 
-    public DynamicProxy(Object target) {
+    public JDKDynamicProxy(Object target) {
         this.target = target;
     }
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         before();
-        Object result = method.invoke(target, args);
+        Object result = method.invoke(target, args);/*result为通过反射执行方法后的返回值*/
         after();
         return result;
     }
