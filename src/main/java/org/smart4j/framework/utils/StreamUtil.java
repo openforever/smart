29241ef959cbc4f888f80e8c037a87e17ceed8ae
openforever <youlovemezky@gmail.com>
2016-04-18 08:01:57 +0800

    流操作工具类，从输入流中获取字符串

---
+++ src/main/java/org/smart4j/framework/utils/StreamUtil.java
@@ -0,0 +1,37 @@
+package org.smart4j.framework.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * Created by snow on 2016/4/17.
+ * 流操作工具类
+ * @author openforever
+ * @since 1.0.0
+ */
+public final class StreamUtil {
+    private static final Logger LOGGER = LoggerFactory.getLogger(StreamUtil.class);
+
+    /**
+     * 从输入流中获取字符串
+     */
+    public static String getString(InputStream is){
+        StringBuilder sb = new StringBuilder();
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String line = null;
+            while ((line = br.readLine()) != null ){
+                sb.append(line);
+            }
+        } catch (IOException e) {
+            LOGGER.error("get string failure", e);
+            throw new RuntimeException(e);
+        }
+        return sb.toString();
+    }
+}
